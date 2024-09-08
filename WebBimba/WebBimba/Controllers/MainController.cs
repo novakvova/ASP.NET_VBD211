49f695fff15365d981b05b63d47209277331f386
novakvova <novakvova@gmail.com>
2024-09-08 15:22:47 +0300

    Begin ASP.NET

---
+++ WebBimba/WebBimba/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBimba.Controllers
+{
+    public class MainController : Controller
+    {
+        //метод у контролері називаться - action - дія
+        public IActionResult Index()
+        {
+            //return "Моніка - привіт!";
+            return View();
+        }
+    }
+}
