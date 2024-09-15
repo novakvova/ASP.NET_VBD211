12d8ddf2f41af0728fb8e4600baa04e946c5858f
novakvova <novakvova@gmail.com>
2024-09-15 17:51:44 +0300

    View categories

--- WebBimba/WebBimba/Controllers/MainController.cs
+++ WebBimba/WebBimba/Controllers/MainController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebBimba.Data;
 
 namespace WebBimba.Controllers
 {
     public class MainController : Controller
     {
+        private readonly AppBimbaDbContext _dbContext;
+
+        //DI - Depencecy Injection
+        public MainController(AppBimbaDbContext context)
+        {
+            _dbContext = context;
+        }
+
         //метод у контролері називаться - action - дія
         public IActionResult Index()
         {
-            //return "Моніка - привіт!";
-            return View();
+            var model = _dbContext.Categories.ToList();
+            return View(model);
         }
     }
 }
--- WebBimba/WebBimba/Program.cs
+++ WebBimba/WebBimba/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebBimba.Data;
+using WebBimba.Data.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +26,46 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Main}/{action=Index}/{id?}");
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetService<AppBimbaDbContext>();
+
+    // Apply migrations if they are not applied
+    context.Database.Migrate(); //автоматичний запуск міграцій на БД, якщо їх там немає
+
+    if (!context.Categories.Any())
+    {
+        var kovbasa = new CategoryEntity
+        {
+            Name = "Ковбаси",
+            Image = "https://rivnepost.rv.ua/img/650/korisnoi-kovbasi-ne-buvae-hastroenterolohi-nazvali_20240612_4163.jpg",
+            Description = "Тим часом відмовлятися від ковбаси повністю не обов’язково. " +
+            "Важливо пам’ятати, що це делікатес, який можна вживати не більше 50 грамів на день."
+        };
+
+        var cheese = new CategoryEntity
+        {
+            Name = "Сири",
+            Image = "https://www.vsesmak.com.ua/sites/default/files/styles/large/public/field/image/syrnaya_gora_5330_1900_21.jpg?itok=RPUrRskl",
+            Description = "Cир – один з найчастіших гостей на нашому столі. " +
+            "Адже це і смачно, і корисно, і доступно. Не можна сказати, що увесь, " +
+            "що продається на прилавках супермаркетів твердий сир – неякісний."
+        };
+
+        var bread = new CategoryEntity
+        {
+            Name = "Хліб",
+            Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Assorted_bread.jpg/420px-Assorted_bread.jpg",
+            Description = "У сегменті ринку «здорового харчування» існують сорти хліба, " +
+            "які майже не сприяють набору зайвої ваги – наприклад, цільнозерновий хліб."
+        };
+
+        context.Categories.Add(kovbasa);
+        context.Categories.Add(cheese);
+        context.Categories.Add(bread);
+        context.SaveChanges();
+    }
+}
 
 app.Run();
