c65bedfaf458af200c5b17c7533d463581573337
novakvova <novakvova@gmail.com>
2024-09-22 15:04:40 +0300

    Begin craeate form

--- WebBimba/WebBimba/Controllers/MainController.cs
+++ WebBimba/WebBimba/Controllers/MainController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebBimba.Data;
+using WebBimba.Models.Category;
 
 namespace WebBimba.Controllers
 {
@@ -17,6 +18,22 @@ namespace WebBimba.Controllers
         public IActionResult Index()
         {
             var model = _dbContext.Categories.ToList();
+            return View(model);
+        }
+
+        [HttpGet] //це означає, що буде відображатися сторінки для перегляду
+        public IActionResult Create()
+        {
+            //Ми повертає View - пусту, яка відобраєате сторінку де потрібно ввести дані для категорії
+            return View();
+        }
+
+        [HttpPost] //це означає, що ми отримуємо дані із форми від клієнта
+        public IActionResult Create(CategoryCreateViewModel model)
+        {
+            //Збережння в Базу даних інформації
+
+            //Те що ми отримали те і повертаємо назад
             return View(model);
         }
     }
---
+++ WebBimba/WebBimba/Models/Category/CategoryCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBimba.Models.Category;
+
+public class CategoryCreateViewModel
+{
+    [Display(Name = "Назва категорії")]
+    public string Name { get; set; } = String.Empty;
+    //Тип для передачі файлі на сервер - із сторінки хочу отримати файл із <input type="file"/>
+    [Display(Name = "Оберіть фото на ПК")]
+    public IFormFile? Photo { get; set; }
+    [Display(Name = "Короткий опис")]
+    public string Description { get; set; } = string.Empty;
+}
