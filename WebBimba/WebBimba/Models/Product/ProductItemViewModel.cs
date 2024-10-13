e87a07333528fa187cfbddccc08c0a28d5476b57
novakvova <novakvova@gmail.com>
2024-10-13 16:33:53 +0300

    Fix View images sizes

---
+++ WebBimba/WebBimba/Models/Product/ProductItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebBimba.Data.Entities;
+
+namespace WebBimba.Models.Product
+{
+    public class ProductItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; } = String.Empty;
+        public List<string>? Images { get; set; }
+    }
+}
